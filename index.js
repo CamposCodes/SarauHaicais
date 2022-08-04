fbd4316fe643ff9c5b5332ab73be6a6e8575fe32
Gabriel Campos Lima Alves <campos.gabriel@estudante.ufjf.br>
2022-08-04 04:30:30 -0300

    adiciona funções aos botões com js

---
+++ index.js
@@ -0,0 +1,85 @@
+let poema1 = document.querySelector("#poema1");
+let poema2 = document.querySelector("#poema2");
+let poema3 = document.querySelector("#poema3");
+let poema4 = document.querySelector("#poema4");
+let poema5 = document.querySelector("#poema5");
+let poema6 = document.querySelector("#poema6");
+let poema7 = document.querySelector("#poema7");
+let poema8 = document.querySelector("#poema8");
+let poema9 = document.querySelector("#poema9");
+let poema10 = document.querySelector("#poema10");
+let poemas = document.querySelector(".poema");
+
+let btn__poema1 = document.querySelector("#button__poema1");
+let btn__poema2 = document.querySelector("#button__poema2");
+let btn__poema3 = document.querySelector("#button__poema3");
+let btn__poema4 = document.querySelector("#button__poema4");
+let btn__poema5 = document.querySelector("#button__poema5");
+let btn__poema6 = document.querySelector("#button__poema6");
+let btn__poema7 = document.querySelector("#button__poema7");
+let btn__poema8 = document.querySelector("#button__poema8");
+let btn__poema9 = document.querySelector("#button__poema9");
+let btn__poema10 = document.querySelector("#button__poema10");
+
+let loc__btn1 = document.querySelector(".loc__button1");
+let loc__btn2 = document.querySelector(".loc__button2");
+let loc__btn3 = document.querySelector(".loc__button3");
+let loc__btn4 = document.querySelector(".loc__button4");
+let loc__btn5 = document.querySelector(".loc__button5");
+let loc__btn6 = document.querySelector(".loc__button6");
+let loc__btn7 = document.querySelector(".loc__button7");
+let loc__btn8 = document.querySelector(".loc__button8");
+let loc__btn9 = document.querySelector(".loc__button9");
+let loc__btn10 = document.querySelector(".loc__button10");
+
+poema1.style.display = "none";
+poema2.style.display = "none";
+poema3.style.display = "none";
+poema4.style.display = "none";
+poema5.style.display = "none";
+poema6.style.display = "none";
+poema7.style.display = "none";
+poema8.style.display = "none"
+poema9.style.display = "none"
+poema10.style.display = "none"
+
+btn__poema1.addEventListener("click", () => {
+    poema1.style.display = "flex";
+    loc__btn1.style.display = "none";
+})
+btn__poema2.addEventListener("click", () => {
+    poema2.style.display = "flex";
+    loc__btn2.style.display = "none";
+})
+btn__poema3.addEventListener("click", () => {
+    poema3.style.display = "flex";
+    loc__btn3.style.display = "none";
+})
+btn__poema4.addEventListener("click", () => {
+    poema4.style.display = "flex";
+    loc__btn4.style.display = "none";
+})
+btn__poema5.addEventListener("click", () => {
+    poema5.style.display = "flex";
+    loc__btn5.style.display = "none";
+})
+btn__poema6.addEventListener("click", () => {
+    poema6.style.display = "flex";
+    loc__btn6.style.display = "none";
+})
+btn__poema7.addEventListener("click", () => {
+    poema7.style.display = "flex";
+    loc__btn7.style.display = "none";
+})
+btn__poema8.addEventListener("click", () => {
+    poema8.style.display = "flex";
+    loc__btn8.style.display = "none";
+})
+btn__poema9.addEventListener("click", () => {
+    poema9.style.display = "flex";
+    loc__btn9.style.display = "none";
+})
+btn__poema10.addEventListener("click", () => {
+    poema10.style.display = "flex";
+    loc__btn10.style.display = "none";
+})
